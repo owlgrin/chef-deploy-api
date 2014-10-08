a58be7761f36574d7b4b7ed6a07a8b633bed7fba
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-08 15:47:27 +0530

    removed the commented code

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,22 +1,2 @@
-# git '/home/ubuntu/apps/api' do
-# 	repository 'gitlab@gitlab.owlgrin.com:horntell/api.git'
-# 	revision 'master'
-# 	action :sync
-# end
-
-# composer_project '/home/ubuntu/apps/api' do
-# 	action :update
-# end
-
-# git '/home/ubuntu/apps/api/app/config/production' do
-# 	repository 'gitlab@gitlab.owlgrin.com:horntell/configs.git'
-# 	revision 'api'
-# 	action :sync
-# end
-
-# execute 'making api storage writable' do
-# 	command 'sudo chmod -R a+w /home/ubuntu/apps/api/app/storage'
-# end
-
 include_recipe "deploy-laravel"
 include_recipe "deploy-laravel::config"
