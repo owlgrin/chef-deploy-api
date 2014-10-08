fa27cb8020c4a13e894af38b60a916703a03d5b6
Mohit Mamoria <mohit.mamoria@gmail.com>
2014-10-08 16:56:19 +0530

    fixed the cookbook by undoing it from wrapper cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures deploy-api'
 long_description 'Installs/Configures deploy-api'
-version          '0.1.3'
+version          '0.1.5'
 
-depends 		 'deploy-laravel', '>= 0.1.0'
+depends 		 'composer', '~> 1.0.5'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,19 @@
-include_recipe "deploy-laravel"
-include_recipe "deploy-laravel::config"
+git '/home/ubuntu/apps/api' do
+	repository 'gitlab@gitlab.owlgrin.com:horntell/api.git'
+	revision 'master'
+	action :sync
+end
+
+composer_project '/home/ubuntu/apps/api' do
+	action :update
+end
+
+execute "Making '/home/ubuntu/apps/api' storage writable" do
+	command "sudo chmod -R a+w /home/ubuntu/apps/api/app/storage"
+end
+
+git "/home/ubuntu/apps/api/app/config/production" do
+	repository 'gitlab@gitlab.owlgrin.com:horntell/configs.git'
+	revision 'api'
+	action :sync
+end
