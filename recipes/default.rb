ccfaa018b431d227112545075e19448768f11e2a
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-02-27 12:50:01 +0530

    Copy .env file for sensitive information

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,3 +19,12 @@ git "/home/ubuntu/apps/api/app/config/production" do
 	revision 'api'
 	action :sync
 end
+
+# move the .env file to the root of project
+file "/home/ubuntu/apps/api/.env.production.php" do
+  owner 'ubuntu'
+  group 'ubuntu'
+  mode 0755
+  content ::File.open("/home/ubuntu/apps/api/app/config/production/.env.production.php").read
+  action :create
+end
