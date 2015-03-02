4e4e4fe7443a061eb6fba8711e4a650ffd69e34d
Mohit Mamoria <mohit.mamoria@gmail.com>
2015-03-02 17:20:17 +0530

    bumped up the version number

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'mohit.mamoria@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures deploy-api'
 long_description 'Installs/Configures deploy-api'
-version          '0.1.5'
+version          '0.1.6'
 
 depends 		 'composer', '~> 1.0.5'
